3fb364cb1925b8b1ea05dbea102fbb0e4da674f1
jeevanSF3855 <116075351+jeevanSF3855@users.noreply.github.com>
2026-01-16 10:41:32 +0530

    bug(REPORT-480045): addressed the review comments

--- src/controls/report-designer/server-integration/index.js
+++ src/controls/report-designer/server-integration/index.js
@@ -1,5 +1,5 @@
 var isSubmit = true;
-var reportServerUrl = '';
+var domainURL = '';
 
 function formSubmit(args) {
     isSubmit = false;
@@ -19,20 +19,19 @@ $(function () {
     var dataValue = "";
     var apiRequest = new Object({ password: "", userid: "" });
 
-    if (reportServerUrl !== '' && reportServerUrl.length > 0) {
+    if (domainURL !== '' && domainURL.length > 0) {
         $.ajax({
             type: "POST",
-            url: "https://" + reportServerUrl + "/reporting/api/site/site1/get-user-key",
+            url: "https://" + domainURL + "/reporting/api/site/site1/get-user-key",
             data: apiRequest,
             success: function (data) {
-                dataValue = data.Token;
-                var token = data && data.Token ? JSON.parse(dataValue) : null;
+                var token = data && data.Token ? JSON.parse(data.Token) : null;
                 if (!token || !token.token_type || !token.access_token) return renderMessage();
 
                 $("#designer").boldReportDesigner(
                     {
-                        serviceUrl: "https://" + reportServerUrl + "/reporting/reportservice/api/Designer",
-                        reportServerUrl: "https://" + reportServerUrl + "/reporting/api/site/site1",
+                        serviceUrl: "https://" + domainURL + "/reporting/reportservice/api/Designer",
+                        reportServerUrl: "https://" + domainURL + "/reporting/api/site/site1",
                         serviceAuthorizationToken: token.token_type + " " + token.access_token,
                         ajaxBeforeLoad: "onAjaxRequest"
                     });
@@ -49,18 +48,16 @@ $(function () {
 function renderMessage() {
     let designerTag = document.getElementById('designer');
     let container = document.getElementById('sample_error_msg');
+    let textNode = container.querySelector('span');
     if (designerTag) designerTag.style.display = 'none';
-    if (container) {
+    if (container && textNode) {
         container.style.display = 'flex';
-        let textNode = document.createElement('span');
-        textNode.style.width = '60%';
         textNode.textContent = 'This sample uses placeholder credentials. To run it, configure your on-premises server URL, service URL, and valid username, password to obtain the service authorization token.';
-        container.append(textNode);
     }
 }
 
 function onAjaxRequest(args) {
     args.headers.push({
-        Key: 'serverurl', Value: 'https://' + reportServerUrl + '/reporting/api/site/site1'
+        Key: 'serverurl', Value: 'https://' + domainURL + '/reporting/api/site/site1'
     });
 }
